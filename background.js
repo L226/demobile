8dd3bb2c175fd61346e4c06d01eaee100c554c22
Luke Fries <lfries@cozero.com.au>
2018-06-14 13:45:48 +1000

    support wiki-style mobile subdomains

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-		var newUrl = details.url.replace('/m.', '/').replace('/mobile.', '/');
+		var newUrl = details.url.replace('.m.', '.').replace('/m.', '/').replace('/mobile.', '/');
 	    alert("This is a mobile site! Redirecting to standard (" + newUrl + ')');
         chrome.tabs.update({url: newUrl});
-	  }, {url: [{hostPrefix : 'm.'}, {hostPrefix : 'mobile.'}]
+	  }, {url: [{hostPrefix : 'm.'}, {hostPrefix : 'en.m.'}, {hostPrefix : 'mobile.'}]
 	});
 });
