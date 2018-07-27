14f12fc33a3cd676f5178cb4ed0b7999c7c3120e
Luke Fries <lfries@cozero.com.au>
2018-07-27 10:43:27 +1000

    exclude stripe urls

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-	var newUrl = details.url.replace('.m.', '.').replace('/m.', '/').replace('/mobile.', '/');
-    alert("This is a mobile site! Redirecting to standard (" + newUrl + ')');
-    chrome.tabs.update({url: newUrl});
-  }, {url: [{hostPrefix : 'm.'}, {hostPrefix : 'en.m.'}, {hostPrefix : 'mobile.'}]
-});
+	if (details.url.indexOf('stripe') < 0) {
+		var newUrl = details.url.replace('.m.', '.').replace('/m.', '/').replace('/mobile.', '/');
+	    alert("This is a mobile site! Redirecting to standard (" + newUrl + ')');
+	    chrome.tabs.update({url: newUrl});
+    }},
+  	{url: [
+	  	{hostPrefix : 'm.'},
+	  	{hostPrefix : 'mobile.'},
+	  	{hostPrefix : 'en.m.'},
+	  	{hostContains : '.m.'},
+	  	{hostContains : '.en.m.'},
+	  	{hostContains : '.mobile.'}
+  	]}
+);
